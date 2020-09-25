2188d73cf84a97d53a93568fa5716985f33a71fe
Velidonimo <velidon@mail.ru>
2020-09-25 11:14:44 +0300

    Window's done

---
+++ frontend.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+
+
+# Actions =============
+def close():
+    root.destroy()
+# =====================
+
+# Labels ==============
+lbl_per = tk.Label(root, text='Per', font=('Times', 20))
+lbl_per.grid(row=0, column=0)
+
+lbl_hour = tk.Label(root, text='hour:', font=('Times', 20))
+lbl_hour.grid(row=0, column=1)
+
+lbl_day = tk.Label(root, text='day:', font=('Times', 20))
+lbl_day.grid(row=0, column=2)
+
+lbl_week = tk.Label(root, text='week:', font=('Times', 20))
+lbl_week.grid(row=0, column=3)
+
+lbl_mo = tk.Label(root, text='month:', font=('Times', 20))
+lbl_mo.grid(row=0, column=4)
+
+lbl_year = tk.Label(root, text='year:', font=('Times', 20))
+lbl_year.grid(row=0, column=5)
+
+lbl_dol = tk.Label(root, text='$', font=('Times', 20))
+lbl_dol.grid(row=1, column=0)
+
+lbl_rub = tk.Label(root, text='₽', font=('Times', 20))
+lbl_rub.grid(row=2, column=0)
+# =====================
+
+# Entries =============
+vars_dol = ['OOps, wrong index']
+for i in range(1, 6):
+    vars_dol.append(tk.StringVar())
+
+entries_dol = ['OOps, wrong index']
+for i in range(1, 6):
+    entries_dol.append(tk.Entry(root, width=10, textvariable=vars_dol[i], font=('Times', 20)))
+    entries_dol[i].grid(row=1, column=i)
+
+vars_rub = ['OOps, wrong index']
+for i in range(1, 6):
+    vars_rub.append(tk.StringVar())
+
+entries_rub = ['OOps, wrong index']
+for i in range(1, 6):
+    entries_rub.append(tk.Entry(root, width=10, textvariable=vars_rub[i], font=('Times', 20)))
+    entries_rub[i].grid(row=2, column=i)
+# =====================
+
+# Buttons =============
+btn_convert = tk.Button(root, text='Convert', font=('Times', 20), width=30, bg='#ababab')
+btn_convert.grid(row=3, column=2, columnspan=3)
+
+btn_close = tk.Button(root, text='Close', font=('Times', 15), bg='#ababab', command=close)
+btn_close.grid(row=4, column=5)
+# =====================
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
